b4d848ae6ff3ff93e6b1fbb0f205a0f78f77f32e
HP <mega.pe4enka@gmail.com>
2021-10-31 14:41:20 +0300

    added sort-popup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Input from "../components/Input"
 import Categories from "../components/Categories"
 import CartItem from '../components/CartBlock'
-import SortPopup from "../components/SortPopup";
 
 const Home = () => {
+
     return (
         <div className='home'>
             <h1>Поиск</h1>
             <Input />
             <Categories />
             <CartItem />
-            {/*<SortPopup />*/}
         </div>
     )
 }
